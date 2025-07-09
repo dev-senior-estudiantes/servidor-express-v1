c6a76849e06b547fa95fdd60df636ba04680bca7
Luneska Newton <majocausil16@gmail.com>
2025-07-08 23:56:31 -0500

    fix: :fire: Se arreglo la funcion en donde inicia el servidor

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 // Definimos el puerto en el que escuchará el servidor
-const PORT = 3000;
+const port = 3000;
 
 // Importamos las rutas del archivo user.routes.js
 const userRoutes = require('./routes/user.routes');
@@ -13,19 +13,14 @@ const userRoutes = require('./routes/user.routes');
 //importamos el middleware de manejo de errores
 const logger = require('./middlewares/logger');
 
-// USAR el middleware logger para registrar las peticiones
-app.use(logger)
-
-// USAR el middleware express.json para aceptar datos en formato JSON
-app.use(express.json())
-
-// DECIRLE a Express que use las rutas en '/api/users'
-app.use('/api/users', userRoutes)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 // INICIAR el servidor en el puerto definido
 app.listen(port, () => {
   // MOSTRAR en consola que el servidor está corriendo
-  console.log(`Servidor corriendo en http://localhost:${port}`)
+  console.log(` HOLA MUNDO DESDE EL PUERTO ${port}`)
 })
 
 
