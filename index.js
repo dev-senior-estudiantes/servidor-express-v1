96a7ed3c0bcf3e04fab9b783fe7deba3ed6fa820
Luneska Newton <majocausil16@gmail.com>
2025-07-08 23:40:34 -0500

    feat: :zap: ActualizacionTaskMary
    se importaron , definieron , y crearon las variables necesarias para iniciar nuestro servidor , adicionalmente , se hizo la estructura del proyecto

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+// Importamos el modulo express
+const express = require('express');
 
+// Creamos una instancia de express
+const app = express();
 
-const express = require('express')
-const app = express()
-const port = 3000
+// Definimos el puerto en el que escuchará el servidor
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Importamos las rutas del archivo user.routes.js
+const userRoutes = require('./routes/user.routes');
 
+//importamos el middleware de manejo de errores
+const logger = require('./middlewares/logger');
+
+// USAR el middleware logger para registrar las peticiones
+app.use(logger)
+
+// USAR el middleware express.json para aceptar datos en formato JSON
+app.use(express.json())
+
+// DECIRLE a Express que use las rutas en '/api/users'
+app.use('/api/users', userRoutes)
+
+// INICIAR el servidor en el puerto definido
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  // MOSTRAR en consola que el servidor está corriendo
+  console.log(`Servidor corriendo en http://localhost:${port}`)
 })
+
+
+
