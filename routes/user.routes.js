cfb3f60eeae02a948faa8189707dc3666751c95f
Andres Hernandez <andres.hernandez74@est.iudigital.edu.co>
2025-07-14 01:18:28 -0500

    Feat: Mejoras en la documentacion, codigo y pseudocodigo del proyecto

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,12 @@
+// Importamos express y creamos un router para manejar las rutas de usuarios
 const express = require('express');
 const router = express.Router();
-const { getUsers } = require('../controllers/user.controller');
+// Importamos el controlador de usuarios que manejará las peticiones
+const { getUsers } = require('../controllers/user.controllers');
 
-// Ruta GET para obtener usuarios
+// Definimos la ruta GET para obtener la lista de usuarios
+// Esta ruta será accesible a través de /api/users
 router.get('/', getUsers);
 
+// Exportamos el router para usarlo en el archivo principal (index.js)
 module.exports = router;
